c3b6b87c4cb07de9e50f56e3ab53e20ea0722108
Gabriel <22393278@utcancun.edu.mx>
2024-02-05 03:44:56 -0500

    Se agregaron 3 pantallas Generos,Login y Register

--- Screens/GenderScreen.js
+++ Screens/GenderScreen.js
@@ -1,16 +1,70 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; // Si estás usando Expo
 
-function GenderScreen({ navigation }) {
+function GenderSelector({ navigation }) {
+  const [gender, setGender] = useState('male');
+
+  const toggleGender = () => {
+    setGender((prevGender) => (prevGender === 'male' ? 'female' : 'male'));
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Gender Screen</Text>
-      <Button
-        title="Go"
+    <View style={styles.container}>
+      <View style={styles.selectorContainer}>
+        <TouchableOpacity onPress={toggleGender} style={styles.arrow}>
+          <MaterialCommunityIcons name="chevron-left" size={44} color="white" />
+        </TouchableOpacity>
+        
+        <MaterialCommunityIcons
+          name={gender === 'male' ? 'gender-male' : 'gender-female'}
+          size={450}
+          color="white"
+        />
+
+        <TouchableOpacity onPress={toggleGender} style={styles.arrow}>
+          <MaterialCommunityIcons name="chevron-right" size={44} color="white" />
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity
+        style={styles.navigationButton}
         onPress={() => navigation.navigate('Ubication')}
-      />
+      >
+        <Text style={styles.navigationButtonText}>Ir a Ubicación</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
-export default GenderScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00569D', // Este es el color de fondo azul para toda la pantalla
+    justifyContent: 'center', // Alinea el selector y el botón en el centro de la pantalla verticalmente
+  },
+  selectorContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  arrow: {
+    padding: 10,
+  },
+  navigationButton: {
+    backgroundColor: '#318CE7', // Un azul más fuerte para el botón
+    borderRadius: 10,
+    paddingHorizontal: 40, // Ancho del botón
+    paddingVertical: 12, // Altura del botón
+    alignItems: 'center', // Asegura que el texto esté centrado horizontalmente
+    justifyContent: 'center', // Centra verticalmente el texto en el botón
+    marginVertical: 30, // Espacio vertical desde el selector de género
+    alignSelf: 'center', // Alinea el botón en el centro horizontalmente
+  },
+  navigationButtonText: {
+    color: 'white',
+    fontSize: 18,
+  },
+});
+
+export default GenderSelector;
--- Screens/RegisterScreen.js
+++ Screens/RegisterScreen.js
@@ -1,16 +1,90 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Button } from 'react-native';
 
 function RegisterScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Register Screen</Text>
-      <Button
-        title="Go"
-        onPress={() => navigation.navigate('Gender')}
-      />
+    <View style={styles.container}>
+      <View style={styles.loginContainer}>
+        <Text style={styles.title}>Registrarse</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Nombre"
+          autoCapitalize="none"
+          placeholderTextColor="#fff"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholderTextColor="#fff"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          secureTextEntry
+          placeholderTextColor="#fff"
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={() => {}}>
+            <Text style={styles.buttonText}>¡Comienza ahora!</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.navigationContainer}>
+        <Button
+          title="Go"
+          onPress={() => navigation.navigate('Gender')}
+          color="#000"
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00569D', // Este es el color de fondo azul para toda la pantalla
+  },
+  loginContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 55,
+    fontWeight: 'bold',
+    color: '#fff',
+    marginBottom: 50, // Aumentado para más espacio
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: '#b5bac9',
+    marginBottom: 30,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    marginTop: 20, // Ajusta el espacio sobre el botón
+    width: '80%', // Ancho del botón
+    alignSelf: 'center', // Centra el botón horizontalmente
+  },
+  button: {
+    paddingVertical: 12, // Altura del botón
+    backgroundColor: '#318CE7', // Un azul más fuerte
+    borderRadius: 10,
+    alignItems: 'center', // Asegura que el texto esté centrado
+    justifyContent: 'center', // Centra verticalmente el texto en el botón
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  navigationContainer: {
+    paddingBottom: 20, // Aumenta este valor para mover el botón hacia arriba
+  }
+});
 
 export default RegisterScreen;
