13c5c19fb01fde1fba1a0817e6125854434c17b2
0-EWT-0 <iamewt@gmail.com>
2024-03-11 10:48:35 -0400

    Todos los archivos agregados y completos

--- BackFlushFinder/src/routes/appRoutes.js
+++ BackFlushFinder/src/routes/appRoutes.js
@@ -2,15 +2,17 @@ import { Router } from 'express';
 import { obtenerEdificios } from '../controllers/edificiosControllers.js';
 import { obtenerBano } from '../controllers/banosController.js';
 import { loginConserje } from '../controllers/conserjesController.js';
+import Usuario from '../models/usuario.js';
+import { registrarUsuario } from '../controllers/usuariosControllers.js';
 
 
 const router = Router();
 
-
+router.post('/registeruser', registrarUsuario)
 router.get('/edificios', obtenerEdificios); // Ruta edificios 
 router.post('/login', loginConserje); // Ruta conserjes
 router.get('/edificios/:edificioId/banos/:banoId', obtenerBano); //Ruta banos por id 
 
-
 export default router;
+
 
